3669197c28bcb7ae18331b68cec3c32295d86410
Enja-Anderson <110401060+Enja-Anderson@users.noreply.github.com>
2022-08-10 09:11:42 -0600

    Mongoose set up

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+//MONGOOSE SETUP
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+
 
 let Person;
 
